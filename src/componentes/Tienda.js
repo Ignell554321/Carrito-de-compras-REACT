b8a88c0e1749f63dee7bef0926716b2c994ac0ba
unknown <miguelguevara_94@outlook.com>
2022-10-26 10:57:39 -0500

    iniciando proyecto carrito de compras

---
+++ src/componentes/Tienda.js
@@ -0,0 +1,25 @@
+export const Tienda=()=>{
+
+    return(
+
+        <div className="container">
+
+            <div className="row">
+
+                <div className="col-md-6">
+
+
+                </div>
+
+                <div className="col-md-6">
+
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+    )
+}
